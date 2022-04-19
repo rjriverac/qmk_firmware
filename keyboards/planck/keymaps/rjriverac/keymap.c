934df49b1464779c1337b53700a8e32415e4b019
Ricardo Rivera-Cabrera <ricardojriveracabrera@gmail.com>
2022-04-19 15:04:23 +0200

    updated config

--- keyboards/planck/keymaps/rjriverac/keymap.c
+++ keyboards/planck/keymaps/rjriverac/keymap.c
@@ -98,7 +98,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Lower
  * ,-----------------------------------------------------------------------------------.
- * |   ~  |  F1  |  F2  |  F3  |  F4  |NUMLCK| HOME |   7  |   8  |   9  |   -  | Bksp |
+ * |   ~  |  F1  |  F2  |  F3  |  F4  |NUMLCK| HOME |   7  |   8  |   9  |   -  |Delete|
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | Del  |  F5  |  F6  |  F7  |  F8  |  Del | Pg Up|   4  |   5  |   6  |   +  |  /   |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
@@ -108,7 +108,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER] = LAYOUT_planck_grid(
-    KC_TILD, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_NLCK, KC_HOME, KC_P7,    KC_P8,    KC_P9,   KC_PMNS, KC_BSPC,
+    KC_TILD, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_NLCK, KC_HOME, KC_P7,    KC_P8,    KC_P9,   KC_PMNS, KC_DEL,
     KC_DEL,  KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_DEL,  KC_PGUP, KC_P4,    KC_P5,    KC_P6,   KC_PPLS, KC_PSLS,
     _______, KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_END,  KC_PGDN, KC_P1,    KC_P2,    KC_P3,   KC_PDOT, KC_PAST,
     _______, _______, _______, _______, _______, _______, _______, _______,  KC_P0,    KC_VOLD, KC_PEQL, KC_PENT
@@ -120,15 +120,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | Tab  |  ALT |   C  |   V  |   A  |PRSCR | Pause|   _  |   =  |   [  |   ]  |  | \ |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Shift|LCTRL |  Tab |  F9  |  F10 |RGCTRL| MENU |ISO # |ISO / |Pg Up |Pg Dn | CAPS |
+ * | Shift|LCTRL |  Tab |  F9  |  F10 |RGCTRL| MENU |ISO # |ISO / |Pg Up |Pg Dn | Shift|
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |      |             |      | Next | Vol- | Vol+ | Play |
  * `-----------------------------------------------------------------------------------'
  */
 [_RAISE] = LAYOUT_planck_grid(
     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
-    _______, KC_RALT, KC_C,    KC_V,    KC_A,   KC_PSCR, KC_PAUS, KC_INS,  KC_EQL,   KC_LBRC,  KC_RBRC,  KC_BSLS,
-    _______, KC_LCTL, KC_TAB,  KC_F9,   KC_F10,  KC_RCTL, KC_APP,  KC_NUHS, KC_NUBS, KC_PGUP, KC_PGDN, KC_CLCK,
+    _______, KC_RALT, KC_C,    KC_V,    KC_A,   KC_PSCR, KC_PAUS, KC_MINS,  KC_EQL,   KC_LBRC,  KC_RBRC,  KC_BSLS,
+    _______, KC_LCTL, KC_TAB,  KC_F9,   KC_F10,  KC_RCTL, KC_APP,  KC_NUHS, KC_NUBS, KC_PGUP, KC_PGDN, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
 ),
 
@@ -157,7 +157,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |MUSmod|Aud on|Audoff|AGnorm|AGswap|Qwerty|Colemk|Dvorak|Plover|      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |Voice-|Voice+|Mus on|Musoff|MIDIon|MIDIof|TermOn|TermOf|      |      |      |
+ * |      |Voice-|Voice+|Mus on|Musoff|MIDIon|MIDIof|TermOn|TermOf|      |      | CAPS |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |      |             |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
@@ -165,7 +165,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_ADJUST] = LAYOUT_planck_grid(
     _______, RESET,   DEBUG,   RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD,  RGB_VAI, RGB_VAD, KC_DEL ,
     _______, _______, MU_MOD,  AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, QWERTY,  COLEMAK,  DVORAK,  PLOVER,  _______,
-    _______, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  TERM_ON, TERM_OFF, _______, _______, _______,
+    _______, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  TERM_ON, TERM_OFF, _______, _______, KC_CLCK,
     _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______
 )
 
